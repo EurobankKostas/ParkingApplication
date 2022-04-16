c470e76dff70a755a090fbea2480ccd6206c889c
Kostas <kostascharalambous@outlook.com.gr>
2022-04-16 12:43:00 +0300

    Adding Booking Component

---
+++ src/component/BookingSection.js
@@ -0,0 +1,9 @@
+export default function  BookingSection(props) {
+
+    return (
+
+        <div style={props.booking ? {} : {display:'none'}} className="col-md-4 col-lg-4 col-sm-12">
+           fksdkfsf
+        </div>
+    )
+}
